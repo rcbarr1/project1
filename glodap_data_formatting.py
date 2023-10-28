ea5f79aedf45dfeb79eee64f648b31d751dae8f3
Reese Barrett <rcbarr1@uw.edu>
2023-10-27 17:12:46 -0700

    Updated glodap_data_formatting.py with new headers and salinity filter, call_ESPERs now calls all 3 for all 16 equations but did not have time yet to test or format output

--- glodap_data_formatting.py
+++ glodap_data_formatting.py
@@ -14,6 +14,7 @@ Description:
     - Filters data to keep only GO-SHIP cruises
     
 To-Do:
+    - filter rows with S > 50 or < 5
     - subset for GO-SHIP cruises
     - finalize formatting of output file (drop cruise columns once GO-SHIP complete)
 """
@@ -42,41 +43,43 @@ glodap = glodap[glodap['G2year'] != np.nan] # get rid of rows with no year
 glodap = glodap[glodap['G2month'] != np.nan] # get rid of rows with no month
 glodap = glodap[glodap['G2day'] != np.nan] # get rid of rows with no day
 glodap['G2hour'] = glodap['G2hour'].replace(np.nan,0) # replace NaN hour with 0
-glodap['G2minute'] = glodap['G2minute'].replace(np.nan,0)# replace NaN minute with 0
-glodap = glodap.reset_index(drop=True)
+glodap['G2minute'] = glodap['G2minute'].replace(np.nan,0) # replace NaN minute with 0
+glodap = glodap[(glodap['G2salinity'] >= 5) & (glodap['G2salinity'] <= 50)] # filter data with salinity > 50 or < 5 (ESPERs do not support this)
+glodap = glodap.reset_index(drop=True) # reset index
 
 # %% select relevant columns and rename
 
 # select columns
 glodap = glodap[['G2expocode','G2cruise','G2station','G2region',
-                               'G2cast','G2year','G2month','G2day','G2hour',
-                               'G2minute','G2latitude','G2longitude','G2depth',
-                               'G2theta','G2salinity','G2oxygen','G2nitrate',
-                               'G2silicate','G2phosphate','G2talk',
-                               'G2phtsinsitutp']]
+                 'G2cast','G2year','G2month','G2day','G2hour',
+                 'G2minute','G2latitude','G2longitude','G2depth',
+                 'G2theta','G2salinity','G2oxygen','G2nitrate',
+                 'G2silicate','G2phosphate','G2talk',
+                 'G2phtsinsitutp']]
     
 
 # rename columns
 
 glodap = glodap.rename(columns={'G2expocode':'Expo Code',
                                 'G2cruise':'Cruise',
-                                'G2station':'G2region',
+                                'G2station':'Station',
+                                'G2region':'Region',
                                 'G2cast':'Cast',
                                 'G2year':'Year',
                                 'G2month':'Month',
                                 'G2day':'Day',
                                 'G2hour':'Hour',
                                 'G2minute':'Minute',
-                                'G2latitude':'Latitude (ºN)',
-                                'G2longitude':'Longitude (ºE)',
-                                'G2depth':'Depth (m)',
-                                'G2theta':'Theta (ºC)',
-                                'G2salinity':'Salinity (PSU)',
-                                'G2oxygen':'Oxygen (μmol/kg)',
-                                'G2nitrate':'Nitrate (μmol/kg)',
-                                'G2silicate':'Silicate (μmol/kg)',
-                                'G2phosphate':'Phosphate (μmol/kg)',
-                                'G2talk':'TA (μmol/kg)',
+                                'G2latitude':'Latitude',
+                                'G2longitude':'Longitude',
+                                'G2depth':'Depth',
+                                'G2theta':'Theta',
+                                'G2salinity':'Salinity',
+                                'G2oxygen':'Oxygen',
+                                'G2nitrate':'Nitrate',
+                                'G2silicate':'Silicate',
+                                'G2phosphate':'Phosphate',
+                                'G2talk':'TA',
                                 'G2phtsinsitutp':'pH'})
 
 # %% convert datetime to decimal time
@@ -113,4 +116,4 @@ glodap = glodap.drop(['Year','Month','Day','Hour','Minute'], axis=1)
 # delete cruise information
 # add this after 
 
-glodap.to_csv(filepath + output_file_name)
+glodap.to_csv(filepath + output_file_name, index=False)
